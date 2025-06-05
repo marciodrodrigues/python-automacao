0ec156c317619b9db71d561ba57e99c734567949
marciodrodrigues <4marciodrodrigues@gmail.com>
2025-06-05 14:02:00 -0300

    leitura do banco de dados e cadastro de todos os produtos, atualizado no arquivo

--- codigo.py
+++ codigo.py
@@ -12,6 +12,7 @@ pyautogui.press("win")
 pyautogui.write("chrome ")
 pyautogui.press("enter")
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+#2
 pyautogui.press("enter")
 time.sleep(3)
 pyautogui.click(x=700, y=370)
@@ -22,3 +23,43 @@ pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(3)
 
+print(pyautogui.position())
+3#
+import pandas as pd
+basedados = pd.read_csv("produtos.csv")
+print(basedados)
+
+4#
+'''pyautogui.click(x=700, y=260)
+codigo = "MOLO000251"
+pyautogui.write(codigo)'''
+
+#5
+for linha in basedados.index:
+    pyautogui.click(x=700, y=260)
+    codigo = basedados.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+    marca = basedados.loc[linha, "marca"]
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+    tipo = basedados.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+    categoria = str(basedados.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+    preco_unitario = str(basedados.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+    custo = str(basedados.loc[linha, "custo"])
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+    obs = str(basedados.loc[linha, "obs"])
+
+    if obs != "nan":
+        pyautogui.write(obs)
+    
+    pyautogui.press("tab")
+    pyautogui.press("enter") 
+    pyautogui.scroll(10000)
